807232d2459f3082ffd1316ac85590f74aa19ddd
ann-jrti <asog.jrti@gmail.com>
2021-07-25 02:13:22 +0200

    cantar linea

--- bingo-pro.js
+++ bingo-pro.js
@@ -1,41 +1,60 @@
 /* checkear que no se repitan números */
-const generateRandomNumber = () => {
-    let arr = []
-    while (arr.length < 16) {
-        let randomNumber = Math.floor(Math.random()* 99);
+const generate15RandomNumbers = () => {
+    const arr = []
+    while (arr.length < 15) {
+        let randomNumber = generateRandomNumber();
         let numCheck = arr.find(number => number === randomNumber);
         if (numCheck !== randomNumber && randomNumber !== 0) arr.push(randomNumber);
-        // if (numCheck !== '' && randomNumber !== 0) arr.push(randomNumber);
     }
     return arr;
 }
 
-let carton = generateRandomNumber();
 
-let bingoCard = [
+const generateRandomNumber = () => Math.floor(Math.random()* 99)
 
-    { number: carton[0], matched: false },
-    { number: carton[1], matched: false },
-    { number: carton[2], matched: false },
-    { number: carton[3], matched: false },
-    { number: carton[4], matched: false },
+let card = generate15RandomNumbers();
 
-    //next line
-    { number: carton[5], matched: false },
-    { number: carton[6], matched: false },
-    { number: carton[7], matched: false },
-    { number: carton[8], matched: false },
-    { number: carton[9], matched: false },
+let bingoCard = card.map(e => {
+    return {
+        number: e, matched: false
+    }
+})
 
-    //next line
-    { number: carton[10], matched: false },
-    { number: carton[11], matched: false },
-    { number: carton[12], matched: false },
-    { number: carton[13], matched: false },
-    { number: carton[14], matched: false },
-
-];
+const asksPlayerName = () => {
+    let playerName = window.prompt('What is your name?')
+}
 
 
-console.log(bingoCard)
 console.table(bingoCard)
+
+const nextTurn = () => {
+    let newRandomNum = generateRandomNumber();
+    let nextNumber = window.confirm(newRandomNum);
+    if (nextNumber) {
+        let indexNumber = card.findIndex(element => element === newRandomNum)
+        if (indexNumber !== -1) {
+            console.log(`You've found the number ${card[indexNumber]}`)
+            bingoCard[indexNumber].number = 'X'
+            bingoCard[indexNumber].matched = true;  
+        }
+    console.table(bingoCard)
+    }
+}
+
+const getLine = (bingoCard, lineNumber) => {
+    const offset = 5 * lineNumber
+    return bingoCard.slice(offset, 4 + offset)
+} 
+
+const cantarLinea = () => {
+    for (let i = 0; i < 3; i++) {
+        const line = getLine(bingoCard, i);
+        let unmatched = line.find(e => e.matched === false);
+        if (!unmatched) console.log('línea!');
+    }
+}
+
+nextTurn() 
+
+let numbers = new Array(99).fill(0).map((e, i) => i + 1)
+
