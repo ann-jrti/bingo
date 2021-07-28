340680de5391594d7870cb8dec724ab419f62707
ann-jrti <asog.jrti@gmail.com>
2021-07-28 23:08:16 +0200

    function check number fixed

--- bingo-pro.js
+++ bingo-pro.js
@@ -1,60 +1,78 @@
-/* checkear que no se repitan números */
+//generates all bingo numbers
+let allBingoNumbers = new Array(99).fill(0).map((e, i) => i + 1)
+
+//generates 15 random numbers for the card
 const generate15RandomNumbers = () => {
-    const arr = []
-    while (arr.length < 15) {
-        let randomNumber = generateRandomNumber();
-        let numCheck = arr.find(number => number === randomNumber);
-        if (numCheck !== randomNumber && randomNumber !== 0) arr.push(randomNumber);
+    let cardNumbers = []
+    while (cardNumbers.length < 15) {
+        let randomNumber = Math.floor(Math.random()* 99)
+        let numCheck = cardNumbers.find(number => number === randomNumber);
+        if (numCheck !== randomNumber && randomNumber !== 0) cardNumbers.push(randomNumber);
     }
-    return arr;
+    return cardNumbers;
 }
 
-
-const generateRandomNumber = () => Math.floor(Math.random()* 99)
-
+//asigns numbers to the bingo card
 let card = generate15RandomNumbers();
 
+//formates bingo card 
 let bingoCard = card.map(e => {
     return {
         number: e, matched: false
     }
 })
+ 
+ //pull a bingo number
+ const pullRandomBingoNumber = () => {
+    let indexNum = Math.floor(Math.random() * allBingoNumbers.length);
+     let removedNum = allBingoNumbers.splice(indexNum, 1)
+     return removedNum[0]
+ }
 
-const asksPlayerName = () => {
+ //asks player username
+ const asksPlayerName = () => {
     let playerName = window.prompt('What is your name?')
 }
 
-
-console.table(bingoCard)
-
-const nextTurn = () => {
-    let newRandomNum = generateRandomNumber();
-    let nextNumber = window.confirm(newRandomNum);
-    if (nextNumber) {
-        let indexNumber = card.findIndex(element => element === newRandomNum)
-        if (indexNumber !== -1) {
-            console.log(`You've found the number ${card[indexNumber]}`)
-            bingoCard[indexNumber].number = 'X'
-            bingoCard[indexNumber].matched = true;  
-        }
+//checks if number is in the player's card
+const checkNumber = () => {
+    const newRandomNum = pullRandomBingoNumber();
+    let showNextNum = window.confirm(newRandomNum);
+    if (showNextNum) {
+        const indexNum = card.indexOf(newRandomNum);
+        if (indexNum !== -1) {
+            console.log(`You've found the number ${card[indexNum]}!`)
+            bingoCard[indexNum].number = 'X';
+            bingoCard[indexNum].matched = true;  
+        } 
+    } 
+    getLine(bingoCard, 0)
+    cantarLinea()
     console.table(bingoCard)
+}
+
+
+const keepDoingIt = () => {
+    while (!card.forEach(element => element === 'X')) {
+        checkNumber()
     }
 }
 
+//generate line
 const getLine = (bingoCard, lineNumber) => {
-    const offset = 5 * lineNumber
-    return bingoCard.slice(offset, 4 + offset)
+    const offset = 5 * lineNumber;
+    return bingoCard.slice(offset, 4 + offset);
 } 
 
+//checks if user completed any line
 const cantarLinea = () => {
     for (let i = 0; i < 3; i++) {
         const line = getLine(bingoCard, i);
         let unmatched = line.find(e => e.matched === false);
-        if (!unmatched) console.log('línea!');
+        if (!unmatched) window.alert('LÍNEA!');
     }
 }
 
-nextTurn() 
 
-let numbers = new Array(99).fill(0).map((e, i) => i + 1)
 
+keepDoingIt()
